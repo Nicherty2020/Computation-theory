d5546ab3ca711450bed114fbbab827cbc7ca367c
Nicolas Gil Hernandez <60415658+Nicherty2020@users.noreply.github.com>
2021-05-13 00:13:33 -0500

    Codigo

---
+++ Taller 3/Taller 3/src/main/java/tc/pokedex/Evolution.java
@@ -0,0 +1,61 @@
+package tc.pokedex;
+
+public class Evolution {
+    String num;
+    String name;
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((num == null) ? 0 : num.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Evolution other = (Evolution) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (num == null) {
+            if (other.num != null)
+                return false;
+        } else if (!num.equals(other.num))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "[name=" + name + ", num=" + num + "]";
+    }
+
+    
+
+}
